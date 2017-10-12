6bda7c9dfb5f198b79827d78b2cd3c6876d3cd74
Andrei Chabokha <achabohka@hotmail.com>
2017-10-12 21:53:15 +0700

    upgraded to run on net core 2; added  a smaple app.

---
+++ BitfinexSample/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using BitfinexApi;
+
+namespace BitfinexSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string key = "your_key"; string secret = "your_secret";
+
+            BitfinexApiV1 api = new BitfinexApiV1(key, secret);
+
+            BalancesResponse bal = api.GetBalances();
+        }
+    }
+}
